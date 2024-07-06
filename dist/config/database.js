00f2ec47bb0256c46e229a1dfc845d232dcd0751
NoeOnDev <alxg5516@gmail.com>
2024-07-06 14:51:55 -0600

    chore: Update database configuration and connection

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,12 +1,34 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.pool = void 0;
+exports.connect = connect;
 const pg_1 = require("pg");
 const env_1 = require("./env");
-exports.pool = new pg_1.Pool({
+const pool = new pg_1.Pool({
     host: env_1.env.db.host,
     port: Number(env_1.env.db.port),
     user: env_1.env.db.user,
     password: env_1.env.db.password,
     database: env_1.env.db.database,
 });
+exports.pool = pool;
+function connect() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield pool.connect();
+            console.log("Database connected");
+        }
+        catch (error) {
+            console.error("Database connection error", error);
+        }
+    });
+}
--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,28 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const database_1 = require("./config/database");
 const env_1 = require("./config/env");
 const app = (0, express_1.default)();
 const port = env_1.env.port;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+function start() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield (0, database_1.connect)();
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
+    });
+}
+start();
