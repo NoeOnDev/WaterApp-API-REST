660a4b838a0fc0dc429a29fca2b4fa54f793448a
NoeOnDev <alxg5516@gmail.com>
2024-07-07 01:08:20 -0600

    feat: Add notificationRoutes for handling notification-related endpoints

--- dist/notifications/notificationRoutes.js
+++ dist/notifications/notificationRoutes.js
@@ -1,2 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // src/notifications/notificationRoutes.ts
+const express_1 = require("express");
+const notificationController_1 = require("./notificationController");
+const authenticateJWT_1 = __importDefault(require("../middlewares/authenticateJWT"));
+const router = (0, express_1.Router)();
+router.post('/send', authenticateJWT_1.default, notificationController_1.notificationController.sendNotification);
+exports.default = router;
